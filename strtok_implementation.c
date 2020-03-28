ea25fa3972df562249cf539c14c03e90177389ff
Ozan Ürkmez <arkantos250@gmail.com>
2020-03-28 17:30:38 +0300

    call for the function we are about to implement

--- strtok_implementation.c
+++ strtok_implementation.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
 int main(){
+    // initialize array string
+    char string[] = "This is a sentence with 7 tokens";
+    char *tokenPtr;
+
+    printf("%s\n%s\n\n%s\n",
+    "The string to be tokenized is:", string,
+    "The tokens are:"
+    );
+
+    tokenPtr = str_tok(string, " "); //begin tokenizing sentence
+
+    // continue tokenizing sentence until tokenPtr becomes NULL
+    while(tokenPtr != NULL){
+        printf("%s\n", tokenPtr);
+        tokenPtr = str_tok(NULL, " ");
+    }//end while
 
     return 0;
+}//end main
+
+char* str_tok(char * inp, char * tok){
+
 }
